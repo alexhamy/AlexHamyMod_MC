77b0de7a1f547edcc2ae8ea8083f02020cc2f282
alexhamy <alexander.2.hamilton@uconn.edu>
2025-01-17 20:52:10 -0500

    some funny

---
+++ src/main/java/me/alexhamy/alexhamymod/items/Sword_Phosphophyllite.java
@@ -0,0 +1,13 @@
+package me.alexhamy.alexhamymod.items;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class Sword_Phosphophyllite extends SwordItem {
+
+    public Sword_Phosphophyllite(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+
+    
+}
